61c0fbf57cc1f98471cad6ff79892abb7bddff4c
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-11-01 10:28:29 -0700

    multi threaded automation cause why not

--- Game of Life Shader Automation/src/Main.java
+++ Game of Life Shader Automation/src/Main.java
@@ -1,38 +1,66 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
+    static AtomicInteger currentTarget = new AtomicInteger();
+    static AtomicInteger numFiles = new AtomicInteger();
+    static File folder = new File("Game of Life Shader Automation/all");
+    static File[] listOfFiles;
+    static int MAX_THREADS = 16;
+
+    public static class FileThread extends Thread {
+        public void run() {
+            try {
+                FileRead fileRead = new FileRead();
+
+                for (int i = 0; i > -1; i++) {
+                    int index = currentTarget.getAndIncrement();
+
+                    try {
+                        if (listOfFiles[index].getName().endsWith(".rle")) {
+                            Pattern pattern = fileRead.readFile("Game of Life Shader Automation/all/" + listOfFiles[index].getName());
+
+                            String fileName = pattern.getName();
+                            fileName = fileName.replace("/", "");
+                            fileName = fileName.replace(".rle", "");
+
+                            File myObj = new File("C:\\Users\\joeys\\Desktop\\Shaders\\" + fileName + ".compute");
+
+                            FileWriter myWriter = null;
+                            myWriter = new FileWriter("C:\\Users\\joeys\\Desktop\\Shaders\\" + fileName + ".compute");
+
+                            myWriter.write(fileRead.getFileString());
+                            myWriter.close();
+
+                            System.out.println("Index: " + currentTarget + " File Created");
+                            numFiles.incrementAndGet();
+                        }
+                    } catch (Exception e) {
+                        break;
+                    }
+
+                }
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 
 
     public static void main(String[] args) throws IOException {
+        numFiles.set(0);
+        currentTarget.set(0);
 
-        int numFiles = 0;
+        listOfFiles = folder.listFiles();
 
-        FileRead fileRead = new FileRead();
-
-        File folder = new File("Game of Life Shader Automation/all");
-        File[] listOfFiles = folder.listFiles();
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].getName().endsWith(".rle")) {
-                Pattern pattern = fileRead.readFile("Game of Life Shader Automation/all/" + listOfFiles[i].getName());
-
-                String fileName = pattern.getName();
-                fileName = fileName.replace("/", "");
-                fileName = fileName.replace(".rle", "");
-
-                File myObj = new File("C:\\Users\\joeys\\Desktop\\Shaders\\" + fileName + ".compute");
-
-                FileWriter myWriter = new FileWriter("C:\\Users\\joeys\\Desktop\\Shaders\\" + fileName + ".compute");
-                myWriter.write(fileRead.getFileString());
-                myWriter.close();
-
-                System.out.println("File created");
-                numFiles++;
-            }
+        for (int i = 0; i < MAX_THREADS; i++) {
+            FileThread t = new FileThread();
+            t.start();
         }
+
         System.out.println("Num Files: " + numFiles);
     }
 }
