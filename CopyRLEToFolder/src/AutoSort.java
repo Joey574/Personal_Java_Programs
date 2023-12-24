b202f07daaa2a69e2607f0347e9aac824391a24e
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-12-24 13:34:37 -0800

    gave in, created auto sort, sorted half

---
+++ CopyRLEToFolder/src/AutoSort.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class AutoSort {
+
+    private int loc;
+    private String category = "Misc";
+
+    AutoSort(String temp) throws IOException {
+        String t;
+        try {
+            t  = temp.substring(temp.indexOf("#C"));
+
+            while(t.startsWith("#C")) {
+                if (t.substring(0, t.indexOf("\n")).startsWith("#C https") && !t.substring(0, t.indexOf("\n")).endsWith(".rle")) {
+
+                    URL url = new URL(t.substring(3, t.indexOf("\n")));
+
+                    // Get the input stream through URL Connection
+                    URLConnection con = url.openConnection();
+                    InputStream is = con.getInputStream();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+                    String line;
+
+                    StringBuilder finalString = new StringBuilder();
+
+                    while ((line = br.readLine()) != null) {
+                        finalString.append(line).append("\n");
+                    }
+
+                    try {
+                        if (finalString.toString().contains("Pattern type")) {
+                            line = finalString.toString().substring(finalString.toString().indexOf("Pattern type"));
+                            category = line.substring(line.indexOf("title=\"") + 7, line.indexOf("\">"));
+                        }
+                    } catch(Exception e) {
+                        br.close();
+                        is.close();
+                        break;
+                    }
+                    br.close();
+                    is.close();
+                    break;
+
+                } else {
+                    t = t.substring(t.indexOf("\n") + 1); // go to next line
+                }
+            }
+        } catch (Exception e) {}
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+}
--- CopyRLEToFolder/src/Main.java
+++ CopyRLEToFolder/src/Main.java
@@ -1,37 +1,74 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
     static File folder = new File("Game of Life Shader Automation/all");
     static File[] listOfFiles;
 
+    private static AtomicInteger index = new AtomicInteger();
+
+    private static int THREAD_COUNT = 16;
+
     public static void main(String[] args) throws IOException {
+        index.set(0);
 
-        listOfFiles = folder.listFiles();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            FileCreator t = new FileCreator();
+            t.start();
+        }
+    }
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].getName().endsWith(".rle")) {
-                StringBuilder temp = new StringBuilder();
-                FileReader fr = new FileReader("Game of Life Shader Automation/all/" + listOfFiles[i].getName());
-                Scanner sc = new Scanner(fr);
+    public static class FileCreator extends Thread {
+        @Override
+        public void run() {
+            try {
 
-                temp.append(sc.nextLine()).append("\n");
+                listOfFiles = folder.listFiles();
 
-                if (!temp.toString().startsWith("#N")) {
-                    temp.insert(0, "#N " + listOfFiles[i].getName().substring(0, (listOfFiles[i].getName().length() - 4)) + "\n");
+                for (int i = index.getAndIncrement(); i < listOfFiles.length; i = index.getAndIncrement()) {
+
+                    System.out.println(i);
+
+                    if (listOfFiles[i].getName().endsWith(".rle")) {
+                        StringBuilder temp = new StringBuilder();
+                        FileReader fr = new FileReader("Game of Life Shader Automation/all/" + listOfFiles[i].getName());
+                        Scanner sc = new Scanner(fr);
+
+                        temp.append(sc.nextLine()).append("\n");
+
+                        if (!temp.toString().startsWith("#N")) {
+                            temp.insert(0, "#N " + listOfFiles[i].getName().substring(0, (listOfFiles[i].getName().length() - 4)) + "\n");
+                        }
+
+                        while(sc.hasNext()) {
+                            temp.append(sc.nextLine()).append("\n");
+                        }
+
+                        AutoSort autoSort = new AutoSort(temp.toString());
+
+                        String subFolder = autoSort.getCategory();
+
+                        String directory = "C:\\Users\\joeys\\Desktop\\rleFiles\\" + subFolder;
+
+                        File f = new File(directory);
+
+                        if (!f.exists()) {
+                            f.mkdir();
+                        }
+
+                        FileWriter myWriter = new FileWriter(directory + "\\" + listOfFiles[i].getName().replace(".rle",
+                                "") + ".txt");
+                        myWriter.write(String.valueOf(temp));
+
+                        myWriter.close();
+                        fr.close();
+                        sc.close();
+                    }
                 }
+            } catch (Exception e) {
 
-                while(sc.hasNext()) {
-                    temp.append(sc.nextLine()).append("\n");
-                }
-
-                FileWriter myWriter = new FileWriter("C:\\Users\\joeys\\Desktop\\rleFiles\\" + listOfFiles[i].getName() + ".txt");
-                myWriter.write(String.valueOf(temp));
-
-                myWriter.close();
-                fr.close();
-                sc.close();
             }
         }
     }
